50143edb98c3e59d7fdc84acbaeef3332288fb89
Adria TImm <atimm13@winona.edu>
2020-01-16 12:46:21 -0600

    Initial Commit

---
+++ InventoryManagerRoughDraft/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  InventoryManagerRoughDraft
+//
+//  Created by Timm, Adria M on 1/16/20.
+//  Copyright © 2020 Timm, Adria M. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
